9e79842296f4696db5d8c70b00b1de33ec3b3a9f
tobisamcode <samueloluwatobiloba48@gmail.com>
2024-08-21 06:35:14 +0100

    async js

---
+++ async_javascript/starter/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+const request = new XMLHttpRequest();
