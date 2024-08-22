9cd45ba7132e3d3ae111dea8904455001089c8b9
tobisamcode <samueloluwatobiloba48@gmail.com>
2024-08-22 19:16:09 +0100

    callback hell 💥

--- async_javascript/starter/script.js
+++ async_javascript/starter/script.js
@@ -3,6 +3,36 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
+const getCountry = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
 
-const request = new XMLHttpRequest();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(request.responseText);
+    console.log('data -- ', data);
+
+    const html = `
+    <article class="country">
+        <img class='country__img' src="${data.flag}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣</span>${data.languages[0]?.name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0]['name']
+            }</p>
+        </div>
+    </article>
+  `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('nigeria');
+getCountryData('usa');
